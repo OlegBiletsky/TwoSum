8d5564c0c5f2efc3ca870153db3c590d9c582162
OlegBiletsky <ol.biletsky@gmail.com>
2021-04-19 00:00:45 +0300

    two_sum done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-let a = 5;
-console.log('hei', a)
+let twoSum = function( nums, target ) {
+let result;    
+    for (let i = 0; i < nums.length-1; i++) {
+        for (let k = i+1; k <= nums.length-1; k++) {
+            if ( nums[i] + nums[k] === target ) {
+                result = [i, k];
+                break;
+            }
+        }
+        if ( result === target ) break;
+    }
+    return result;
+};
+
+a = [89, 45, 7, 11, 15, 22, 105, 59, 2];
+b = 9;
+twoSum(a, b);
